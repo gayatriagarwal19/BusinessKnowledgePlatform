9ec362ef29f1ad3c6a10b6c8e3ca7ad244f1b1c1
Gayatri Agarwal <gayatri19agarwal@gmail.com>
2025-08-04 13:54:21 +0530

    deployment fixes

--- frontend/src/utils/setAuthToken.js
+++ frontend/src/utils/setAuthToken.js
@@ -1,6 +1,9 @@
 
 import axios from 'axios';
 
+// Set the base URL for Axios
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api'; // Default to localhost for development
+
 const setAuthToken = (token) => {
   if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
